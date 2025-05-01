0058eed112cdc2f1dfc11b357e77f10f540038fd
Mohit Prakash <mohit_cpr@yahoo.com>
2025-05-01 21:36:48 +0530

    fixed signout issue

--- app/api/auth/signout/route.ts
+++ app/api/auth/signout/route.ts
@@ -1,10 +1,14 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
+import { cookies } from "next/headers";
 
 export async function POST() {
   try {
     // Get the current session
     const session = await getServerSession();
+
+    console.log("Session:", session);
+    const allCookies = cookies(); // get cookies from the request
 
     if (session) {
       // Clear the session
@@ -13,6 +17,13 @@ export async function POST() {
         headers: {
           "Content-Type": "application/json",
         },
+      });
+
+      (await allCookies).getAll().forEach((cookie) => {
+        response.cookies.set(cookie.name, "", {
+          expires: new Date(0),
+          path: "/", // clear from root path
+        });
       });
 
       // Clear the session cookie
@@ -27,15 +38,41 @@ export async function POST() {
         path: "/",
       });
 
+      response.cookies.set("next-auth.callback-url", "", {
+        expires: new Date(0),
+        path: "/",
+      });
+
+      response.cookies.set("__next_hmr_refresh_hash__", "", {
+        expires: new Date(0),
+        path: "/",
+      });
+
+      response.cookies.set("__Secure-next-auth.session-token", "", {
+        path: "/",
+        expires: new Date(0),
+        secure: true,
+        httpOnly: true,
+        sameSite: "lax",
+        domain: "yourdomain.com", // match exactly what was set
+      });
+
       return response;
     }
 
-    return new NextResponse(JSON.stringify({ success: true }), {
+    const response = new NextResponse(JSON.stringify({ success: true }), {
       status: 200,
       headers: {
         "Content-Type": "application/json",
       },
     });
+    (await allCookies).getAll().forEach((cookie) => {
+      response.cookies.set(cookie.name, "", {
+        expires: new Date(0),
+        path: "/", // clear from root path
+      });
+    });
+    return response;
   } catch (error) {
     console.error("Error signing out:", error);
     return new NextResponse(JSON.stringify({ error: "Failed to sign out" }), {
